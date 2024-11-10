0b033c79e2fc291fc55a3664e86ad2cbe4f4a9e8
Biwas Bhandari <157286107+biwasbhandari@users.noreply.github.com>
2024-11-10 10:25:15 +0545

    fix(version): use stable python version 3.10

--- shell.nix
+++ shell.nix
@@ -1,21 +1,21 @@
 { pkgs ? import <nixpkgs> {} }:
 
-   let
-     pythonPackages = pkgs.python3Packages;
-   in pkgs.mkShell {
-     buildInputs = with pkgs; [
-       python3
-       pythonPackages.pip
-       pythonPackages.virtualenv
-       gcc
-       stdenv.cc.cc.lib
-       zlib
-       glib
-       ffmpeg
-     ];
+let
+  pythonPackages = pkgs.python310Packages; # Use Python 3.10 packages
+  python = pkgs.python310; # Specify Python 3.10
+in pkgs.mkShell {
+  buildInputs = with pkgs; [
+    python
+    pythonPackages.pip
+    pythonPackages.virtualenv
+    gcc
+    stdenv.cc.cc.lib
+    zlib
+    glib
+    ffmpeg
+  ];
 
-     shellHook = ''
-       export LD_LIBRARY_PATH=${pkgs.stdenv.cc.cc.lib}/lib:${pkgs.zlib}/lib:$LD_LIBRARY_PATH
-     '';
-   }
-   
+  shellHook = ''
+    export LD_LIBRARY_PATH=${pkgs.stdenv.cc.cc.lib}/lib:${pkgs.zlib}/lib:$LD_LIBRARY_PATH
+  '';
+}
